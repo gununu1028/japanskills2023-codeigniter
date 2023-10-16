8491adbfba93c36bf33e597ad7d489dd625a8c5c
松田淳司 <amatsuda@zaico.co.jp>
2023-10-17 05:30:56 +0900

    指導用に調整

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,19 +7,10 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
+// ここから下を追記
 $routes->group('admin', function ($routes) {
     $routes->get('/', 'AdminController::getLogin');
     $routes->get('login', 'AdminController::getLogin');
     $routes->post('login', 'AdminController::postLogin');
     $routes->get('dashboard', 'AdminController::getDashboard');
-    $routes->group('user', function ($routes) {
-        $routes->get('/', 'AdminController::getUserList');
-        $routes->get('new', 'AdminController::getUserNew');
-        $routes->post('create', 'AdminController::postUserCreate');
-        $routes->get('(:num)', 'AdminController::getUserShow/$1');
-        $routes->get('(:num)/edit', 'AdminController::getUserEdit/$1');
-        $routes->put('(:num)', 'AdminController::putUserUpdate/$1');
-        $routes->patch('(:num)/active_status', 'AdminController::patchUserActiveStatus/$1');
-        $routes->delete('(:num)', 'AdminController::deleteUser/$1');
-    });
 });
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,34 +6,4 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
-    // テーブルを指定
-    protected $table = 'user';
-    // 主キーを指定
-    protected $primaryKey = 'id';
-    // 変更を許可するカラムを指定
-    protected $allowedFields = ['name', 'password', 'active_status'];
-    // 作成日時、更新日時を指定
-    protected $useTimestamps = true;
-
-    // バリデーションルール
-    public function getRules($id = null)
-    {
-        $rules = [
-            'name' => [
-                'rules'  => "required|is_unique[user.name,id,{$id}]",
-                'errors' => [
-                    'required' => 'ユーザー名は必須です。',
-                    'is_unique' => 'このユーザー名は既に使用されています。'
-                ],
-            ],
-            'password' => [
-                'rules'  => 'required|min_length[8]',
-                'errors' => [
-                    'required' => 'パスワードは必須です。',
-                    'min_length' => 'パスワードは8文字以上にしてください。'
-                ],
-            ],
-        ];
-        return $rules;
-    }
 }
