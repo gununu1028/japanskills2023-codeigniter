6c5aa14af7ee4489c603d7b21df0d699ed2a3997
松田淳司 <amatsuda@zaico.co.jp>
2024-10-20 21:22:22 +0900

    講習用に調整

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,7 +7,6 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-// 管理画面
 $routes->group('admin', function ($routes) {
     $routes->get('/', 'AdminController::getLogin');
     $routes->get('login', 'AdminController::getLogin');
@@ -15,7 +14,7 @@ $routes->group('admin', function ($routes) {
     $routes->get('logout', 'AdminController::getLogout');
     $routes->get('dashboard', 'AdminController::getDashboard');
 
-    // イベント管理
+    // ここから追加
     $routes->group('event', function ($routes) {
         $routes->get('/', 'EventController::getEventList');
         $routes->get('new', 'EventController::getEventNew');
@@ -24,18 +23,5 @@ $routes->group('admin', function ($routes) {
         $routes->post('edit/(:id)', 'EventController::postEventEdit/$1');
         $routes->get('delete/(:id)', 'EventController::getEventDelete/$1');
     });
-});
-
-// REST API
-$routes->group('api', function ($routes) {
-    $routes->group('auth', function ($routes) {
-        $routes->post('signup', 'AuthController::postSignup');
-        $routes->post('login', 'AuthController::postLogin');
-        $routes->post('logout', 'AuthController::postLogout');
-    });
-    $routes->group('user', function ($routes) {
-        $routes->get('/', 'UserController::getUserShow');
-        $routes->put('/', 'UserController::putUserUpdate');
-        $routes->delete('/', 'UserController::deleteUser');
-    });
+    // ここまで追加
 });
